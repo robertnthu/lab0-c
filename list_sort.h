9e5f6da62801ac65f00e97448f5a254b15f64b91
robertnthu <k0983357798@gmail.com>
2022-02-28 16:10:22 +0800

    Git add list_sort.c list_sort.h

---
+++ list_sort.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_LIST_SORT_H
+#define _LINUX_LIST_SORT_H
+
+#include <linux/types.h>
+
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+
+struct list_head;
+
+typedef int
+    __attribute__((nonnull(2, 3))) (*list_cmp_func_t)(void *,
+                                                      const struct list_head *,
+                                                      const struct list_head *);
+
+void list_sort(void *priv, struct list_head *head, list_cmp_func_t cmp);
+#endif
